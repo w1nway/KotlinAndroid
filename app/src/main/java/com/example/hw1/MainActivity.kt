d92f8dd61d209d645e4a74d9ceeb296caef497d1
w1nway <90196953+w1nway@users.noreply.github.com>
2022-07-03 01:51:14 +0300

    correct hw1

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -8,41 +8,37 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var nameField: EditText? = null
-    private var heightField: EditText? = null
-    private var weightField: EditText? = null
-    private var ageField: EditText? = null
-    private var mainButton: Button? = null
-    private var textOutput: TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        nameField = findViewById(R.id.et_Name)
-        heightField = findViewById(R.id.et_Height)
-        weightField = findViewById(R.id.et_Weight)
-        ageField = findViewById(R.id.et_Age)
-        mainButton = findViewById(R.id.button)
-        textOutput = findViewById(R.id.textView)
+        val nameField: EditText? = findViewById(R.id.et_name)
+        val heightField: EditText? = findViewById(R.id.et_height)
+        val weightField: EditText? = findViewById(R.id.et_weight)
+        val ageField: EditText? = findViewById(R.id.et_age)
+        val textOutput: TextView? = findViewById(R.id.tv_result)
+        val mainButton: Button? = findViewById(R.id.btn_name)
 
         mainButton?.setOnClickListener {
 
-            val name = nameField?.text.toString()
-            val height = heightField?.text.toString()
-            val weight = weightField?.text.toString()
-            val age = ageField?.text.toString()
+            try {
+                val name = nameField?.text.toString()
+                val height = heightField?.text.toString().toInt()
+                val weight = weightField?.text.toString().toFloat()
+                val age = ageField?.text.toString().toInt()
 
-            if(name == "" || height == "" || weight == "" || age == "")
-                textOutput?.text = "Данные введены некорректно."
-            else {
-                if (name.length > 50 || (height.toInt() <= 0 || height.toInt() >= 250)
-                    || (weight.toInt() <= 0 || weight.toInt() >= 250)
-                    || (age.toInt() <= 0 || age.toInt() >= 150))
+
+                if (name.length > 50 || (height <= 0 || height >= 250)
+                    || (weight <= 0 || weight >= 250)
+                    || (age <= 0 || age >= 150))
                     textOutput?.text = "Данные введены некорректно."
                 else {
-                    var value = name.length + height.toInt() + weight.toInt() + age.toInt()
-                    textOutput?.text ="Ответ:$value"
+                    val value = name.length + height + weight + age
+                    textOutput?.text = "Ответ:$value"
                 }
+
+            } catch (e: Exception) {
+                textOutput?.text = "Данные введены некорректно."
             }
         }
     }
